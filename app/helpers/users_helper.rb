d34c554390ff7401f9b25e9525867335acc9159c
Sheri Zada <sheri.h.zada@gmail.com>
2015-12-07 13:14:03 -0800

    user default taste

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,3 @@
 module UsersHelper
-  def default_taste
-    "\nglow in the dark yoyo,\nglow in the dark socks,\na cute lunchbox,\na hug".html_safe
-  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
   attr_reader :password
-  after_initialize :ensure_session_token
+  after_initialize :ensure_session_token, :ensure_default_taste
 
   validates :email, :session_token, :password_digest,
       :name, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :email, :name, :session_token, uniqueness: true
+
+  def ensure_default_taste
+    self.taste ||= "\nglow in the dark yoyo,
+      \nglow in the dark socks,\na cute lunchbox,\na hug".html_safe
+  end
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
