5802cfafaf66a0f2905adef39f45f26a67512cf9
Claire Rogers <clairekrogers@gmail.com>
2015-12-08 13:08:33 -0800

    mailer fixxxx

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,7 +2,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'https://secret-snowman.herokuapp.com',
+  domain:               'gmail.com',
   user_name:            'aa.secret.snowman',
   password:             'markovandcurie',
   authentication:       'plain',
